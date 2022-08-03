b70d8057ca69082c267a28c25797d77ff5fc7989
ar2r-2r2r <106029214+ar2r-2r2r@users.noreply.github.com>
2022-08-04 01:07:20 +0300

    Delete test directory

--- test/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require "connection.php";
-    unset($_SESSION['logged_user']);         //удаляем сессию
-    header('Location: /');            //пересылаем пользователя на главную страницу
-?>
-
